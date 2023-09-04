fbc32a67bbad073eb93d2faec9e3c5f245b38d6b
badr ghanbi <bghanbi50@gmail.com>
2023-09-04 14:37:07 +0100

    create the fixed navbar

--- main.js
+++ main.js
@@ -53,17 +53,35 @@ prevButton.addEventListener("click", () => {
 const initialNavbarOffsetTop = navbar.offsetTop;
 
 // Function to toggle the "fixed" class based on scroll position
+// function toggleFixedNavbar() {
+//     if (window.pageYOffset >= initialNavbarOffsetTop) {
+//         navbar.classList.add('fixed');
+
+//     } else {
+//         navbar.classList.remove('fixed');
+
+//     }
+// }
+
+// // // Add a scroll event listener
+// window.addEventListener('scroll', toggleFixedNavbar);
 function toggleFixedNavbar() {
-    if (window.pageYOffset >= initialNavbarOffsetTop) {
-        navbar.classList.add('fixed');
-        // navList.classList.add('fixedNavList');
-
+    if (window.innerWidth <= 768) { // Check if window width is less than or equal to 768px
+        if (window.pageYOffset >= initialNavbarOffsetTop) {
+            navbar.classList.add('fixed');
+        } else {
+            navbar.classList.remove('fixed');
+        }
     } else {
+        // If the window width is larger than 768px, you can choose to do something else or nothing.
+        // For example, you can remove the 'fixed' class if it's currently added when the window is resized.
         navbar.classList.remove('fixed');
-        // navList.classList.remove('fixedNavList');
-
     }
 }
 
-// // Add a scroll event listener
+// Add a scroll event listener
 window.addEventListener('scroll', toggleFixedNavbar);
+
+// Add a resize event listener to handle changes in window width
+window.addEventListener('resize', toggleFixedNavbar);
+
